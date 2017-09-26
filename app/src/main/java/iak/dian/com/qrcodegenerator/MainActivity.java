e71b00fe549153b6cd38cab47d014b70218a6508
miscla <feisalfarhanudin@gmail.com>
2017-09-26 11:00:07 +0700

    first commit

---
+++ app/src/main/java/iak/dian/com/qrcodegenerator/MainActivity.java
@@ -0,0 +1,90 @@
+package iak.dian.com.qrcodegenerator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText text_qr_code;
+    Button but_generate;
+    ImageView qr_code;
+    Button but_scanner;
+    TextView tv_hasil;
+    //String text_Qr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text_qr_code = (EditText)findViewById(R.id.et_gen_qr_code);
+        but_generate = (Button)findViewById(R.id.but_gen_qr);
+        but_scanner = (Button)findViewById(R.id.but_scan_qr);
+        tv_hasil = (TextView)findViewById(R.id.tv_hasil);
+        qr_code = (ImageView)findViewById(R.id.qr_code);
+
+        final Activity activity = this;
+
+        but_generate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //text_Qr = text_qr_code.getText().toString().trim();
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                try{
+                    BitMatrix bitMatrix = multiFormatWriter.encode(text_qr_code.getText().toString(), BarcodeFormat.QR_CODE, 200, 200);
+                    BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                    Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                    qr_code.setImageBitmap(bitmap);
+                }
+                catch (WriterException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        but_scanner.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            if(result.getContents()==null){
+                Toast.makeText(this, "Kamu telah keluar", Toast.LENGTH_LONG).show();
+            }else{
+                //Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                tv_hasil.setText(result.getContents());
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+}
